6cb4a6ac58d1888505213887d1d2db080bfdd69b
full-StackDevelopers <rasithshahul@gmail.com>
2025-07-14 23:29:53 +0530

    Real time Chatify app

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import { connectDB } from "./lib/lib.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server Running On port ${PORT}`);
+  connectDB();
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,4 @@
+export const App = () => {
+  return <div className="bg-red-500 h-screen">App</div>;
+};
+export default App;
