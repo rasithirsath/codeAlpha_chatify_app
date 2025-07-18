63b3f5d112c65976b18a81c5fab91bbf330de614
full-StackDevelopers <rasithshahul@gmail.com>
2025-07-18 17:27:35 +0530

    Features added

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,17 +11,20 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
-); // allow cookies to be sent with requests);
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes); // ✅ This was missing
+app.use("/api/chats", chatRoutes); // ✅ This too
 
 app.listen(PORT, () => {
   console.log(`Server Running On port ${PORT}`);
